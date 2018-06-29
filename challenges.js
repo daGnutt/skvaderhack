0ea9479d418d84490fdbbfba30eb19de36f461cc
Gnutt Halvordsson <gnutt@halvordsson.se>
2018-06-29 10:19:07 +0200

    Added challenges application

---
+++ challenges.js
@@ -0,0 +1,45 @@
+
+function fetchHints()
+{
+	var fetcher = new XMLHttpRequest();
+	fetcher.addEventListener( "load", parseHints );
+	fetcher.open( "GET", "api/hints.py" );
+	fetcher.send();
+}
+
+function parseHints()
+{
+	if(this.status != 200)
+	{
+		throw this;
+	}
+	killAllChildren(document.getElementById("hintlist"));
+	var hints = JSON.parse(this.responseText);
+	for(var hintid in hints)
+	{
+		var hint = hints[ hintid ];
+		var hintdom = document.createElement("li");
+		if( hint.url )
+		{
+			var anchor = document.createElement("a");
+			anchor.href = hint.url;
+			anchor.appendChild( document.createTextNode( hint.description ) );
+			hintdom.appendChild( anchor );
+		} else {
+			hintdom.appendChild( document.createTextNode( hint.description ) );
+		}
+		document.getElementById("hintlist").appendChild(hintdom);
+		console.log(hint);
+	}
+	console.log(hints);
+}
+
+function killAllChildren(dom)
+{
+	while(dom.firstChild)
+	{
+		dom.removeChild(dom.firstChild);
+	}
+}
+
+fetchHints();
