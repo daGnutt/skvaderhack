1cbf82b255e926f1c3ab27d1c0d2ea909d936bac
Gnutt Halvordsson <gnutt@halvordsson.se>
2018-07-15 10:58:23 +0200

    Changed function to produce scorelist

--- api/score.py
+++ api/score.py
@@ -5,7 +5,7 @@
 
 import json
 import os
-import sys
+import sqlite3
 
 import key
 
@@ -14,7 +14,51 @@ RETURN_HEADERS = []
 def __do_get():
     RETURN_HEADERS.append('Status: 200')
     scores = key.get_all_points()
-    return json.dumps(scores);
+    return json.dumps(scores)
+
+def scorelist():
+    """Retrieves a calculated list of all groups points"""
+    database = sqlite3.connect("database.sqlite3")
+    claimcursor = database.execute(("SELECT cl.groupname, cl.catchtime,"
+                                    " ke.key, ke.first, ke.second, ke.third, ke.other"
+                                    " FROM claims AS cl INNER JOIN keys AS ke"
+                                    " ON (ke.key == cl.key COLLATE NOCASE)"
+                                    " ORDER BY ke.key ASC, cl.catchtime ASC"))
+    allclaims = claimcursor.fetchall()
+
+    groups = {}
+    key = None
+    for claim in allclaims:
+        _group = claim[0]
+        _key = claim[2]
+        _point1 = claim[3]
+        _point2 = claim[4]
+        _point3 = claim[5]
+        _point = claim[6]
+
+        if _key != key:
+            num_in_key = 0
+            key = _key
+
+        if not _group in groups:
+            groups[_group] = 0
+
+        num_in_key = num_in_key + 1
+
+        if num_in_key == 1:
+            groups[_group] = groups[_group] + _point1
+        elif num_in_key == 2:
+            groups[_group] = groups[_group] + _point2
+        elif num_in_key == 3:
+            groups[_group] = groups[_group] + _point3
+        else:
+            groups[_group] = groups[_group] + _point
+
+    returnvalue = []
+    for group in groups:
+        returnvalue.append({"name": group, "score": groups[group]})
+
+    return returnvalue
 
 def __main():
     if not 'REQUEST_METHOD' in os.environ:
